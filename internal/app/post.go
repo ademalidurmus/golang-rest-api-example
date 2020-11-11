f5248e12144fc9f861dcb4ac915f14da0ac0ed64
Adem Ali Durmus <ademalidrms@gmail.com>
2020-11-11 23:50:04 +0300

    Refactor after new structure

--- internal/app/post.go
+++ internal/app/post.go
@@ -1,1 +1,48 @@
 package app
+
+import (
+	"time"
+
+	"github.com/ademalidurmus/golang-rest-api-example/internal/database/post"
+	"github.com/ademalidurmus/golang-rest-api-example/internal/model"
+)
+
+// Post ...
+type Post struct {
+	PostRepository *post.Repository
+}
+
+// NewPostAPP ...
+func NewPostAPP(p *post.Repository) Post {
+	return Post{PostRepository: p}
+}
+
+// SearchPost ...
+func (p *Post) SearchPost() ([]model.Post, error) {
+	return p.PostRepository.Search()
+}
+
+// CreatePost ...
+func (p *Post) CreatePost(post *model.Post) (model.Post, error) {
+	post.CreatedAt = time.Now().Format(time.RFC3339)
+	post.UpdatedAt = time.Now().Format(time.RFC3339)
+
+	return p.PostRepository.Create(*post)
+}
+
+// ReadPost ...
+func (p *Post) ReadPost(id int) (model.Post, error) {
+	return p.PostRepository.Read(id)
+}
+
+// UpdatePost ...
+func (p *Post) UpdatePost(post *model.Post) (model.Post, error) {
+	post.UpdatedAt = time.Now().Format(time.RFC3339)
+
+	return p.PostRepository.Update(*post)
+}
+
+// DeletePost ...
+func (p *Post) DeletePost(id int) (bool, error) {
+	return p.PostRepository.Delete(id)
+}
--- internal/model/post.go
+++ internal/model/post.go
@@ -1,1 +1,11 @@
 package model
+
+// Post ...
+type Post struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	Status    string `json:"status"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
